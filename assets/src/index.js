753029cf309a4acc9742f3f41a383c1c9b8273ae
Adnaan Badr <badr.adnaan@gmail.com>
2022-05-16 02:58:45 +0200

    use the new goliveview js client

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,10 +1,10 @@
 import {Application} from "@hotwired/stimulus"
 import {definitionsFromContext} from "@hotwired/stimulus-webpack-helpers"
-import {GoliveviewController} from 'goliveview-stimulus';
+import {Dispatcher} from 'goliveview';
 import "./styles.scss";
 
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
-application.register("glv", GoliveviewController)
+application.register("glv", Dispatcher)
 window.Stimulus = application
